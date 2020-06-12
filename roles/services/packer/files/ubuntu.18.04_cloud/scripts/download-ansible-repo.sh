d9dc70df4f1207fe7ff5b5524d32e8481401785b
Michalis Michaloliakos <michalis@hashicorp.com>
2020-06-12 22:01:22 +0100

    CICD-5: Adding a git install and repo download provisioner

---
+++ roles/services/packer/files/ubuntu.18.04_cloud/scripts/download-ansible-repo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e -x
+
+echo 'Get git and ansible codebase on the host.'
+# sudo apt-get update -y
+sudo apt-get install -y git
+
+cd /tmp/packer 
+git clone git@github.com:michnmi/ansible_internal.git 
