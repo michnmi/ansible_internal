90098fb0a9c9365021f5ae76163ddd4c15df36b7
Michalis Michaloliakos <michalis@hashicorp.com>
2020-06-12 21:34:59 +0100

    CICD-5: Typo

--- roles/services/packer/files/ubuntu.18.04_cloud/scripts/install-ansible.sh
+++ roles/services/packer/files/ubuntu.18.04_cloud/scripts/install-ansible.sh
@@ -4,13 +4,10 @@ set -e -x
 
 # Following the official ansible docs: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible-on-ubuntu
 
-# Install Docker CE
-## Set up the repository:
-### Install packages to allow apt to use a repository over HTTPS
 echo 'Installing Ansible on host'
-sudo apt-get update -y
+# sudo apt-get update -y
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -y ansible
 
-ansible -v
+ansible --version
