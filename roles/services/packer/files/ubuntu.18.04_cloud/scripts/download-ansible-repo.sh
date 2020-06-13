5182c1361dafdeda203ef5e0e33ede397209aafa
Michalis Michaloliakos <michalis@hashicorp.com>
2020-06-13 16:47:43 +0100

    CICD-5: Move initial provisioining scripts to be ansible steps and remove debuggin entries

--- roles/services/packer/files/ubuntu.18.04_cloud/scripts/download-ansible-repo.sh
+++ roles/services/packer/files/ubuntu.18.04_cloud/scripts/download-ansible-repo.sh
@@ -2,21 +2,23 @@
 
 set -e -x
 
-echo 'Get git and ansible codebase on the host.'
+
+# Following the official ansible docs: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible-on-ubuntu
+
+echo 'Installing Ansible and git on host'
 # sudo apt-get update -y
-sudo apt-get install -y git
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install -y ansible git
+
+ansible --version
+git version
 
 cd /tmp/
 git clone https://github.com/michnmi/ansible_internal
 
 cd ansible_internal
 git checkout --track origin/CICD-5
-
-ps -ef | grep ubuntu
-id
-whoami
-who am i
-cat /etc/passwd
 
 ansible-playbook  -i inventories/cloud_vms/hosts.ini -l cloud_vm playbooks/cloud_vm.yml
 id michnmi
